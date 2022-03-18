84a0035e40eef278cee606e67d2409c7cea4e35b
Julio <julio.campos@iebtdigital.com.br>
2022-03-18 15:43:12 -0300

    primeiro tweet

--- src/ui/pages/index.jsx
+++ src/ui/pages/index.jsx
@@ -1,12 +1,23 @@
 import { useIndex } from "../../data/hooks/pages/useIndex.page";
+import Tweet from "../components/inputs/data-display/tweet/tweet";
 import TextInput from "../components/inputs/TextInput/TextInput";
 import styles from '../styles/pages/index.module.css';
 
+const tweet = {
+   date: 'há 5 dias' ,
+   text: 'meu primeiro tweet',
+   user: {
+       name: 'Júlio Campos',
+       username: 'juuliocampos',
+       picture: 'https://github.com/juuliocampos.png'
+   }
+}
 export default function Index() {
     useIndex();
     return (
         <div>
-            <TextInput />
+            <TextInput  />
+            <Tweet tweet={tweet}/>
         </div>
     )
 }
